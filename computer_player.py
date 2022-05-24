51e623ed6282ad0d3f7da2e96b466ffcfe9be53e
Paul M Vega <Paul@macbook-pro-2.lan>
2022-05-24 16:31:12 -0500

    created the intial vs code file, created parent class and child classes

---
+++ computer_player.py
@@ -0,0 +1,5 @@
+from player import Player
+
+class AI_player (Player):
+    def __init__(self):
+        super().__init__()
---
+++ gameplay.py
@@ -0,0 +1,2 @@
+from human_player import Human
+from computer_player import AI_player
