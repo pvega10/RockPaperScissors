2c753b0b66b99f67f5bab108f6606f3a40b5159c
Paul M Vega <Paul@macbook-pro-2.lan>
2022-05-30 16:19:59 -0500

    added the win/lose criteria, added a win counter, added a second round

--- gameplay.py
+++ gameplay.py
@@ -9,13 +9,63 @@ class Gameplay:
         pass
 
     def run_game(self):
+        human_counter = 0
+        computer_counter = 0
         print ("Welcome to the RPSLS Arena!")
-        print (input ("How many user controlled players in this game? "))
+        user_input = (input ("How many user controlled players in this game? "))
         self.game_intro()
-        self.players_choose_item()
+        self.players_choose_item_round_one()
+        # self.players_choose_item_round_two()
+        # self.game_showdown()
+        if human_counter == 1:
+            print (f"GAME OVER! {self.human.name} IS VICTORIUS ")
         pass
 
-    def players_choose_item (self):
+    def players_choose_item_round_one (self):
+        print("Round 1 is under way")
+        self.human.choose_item()
+        self.ai.choose_item()
+        if self.human.selected_item == self.ai.selected_item:
+            print ("Its a tie!")
+        elif self.human.selected_item == "Rock" and self.ai.selected_item == "Scissors":
+            human_counter = 1
+            print (f"{self.human.name} WINS!")
+        elif self.human.selected_item == "Paper" and self.ai.selected_item == "Rock":
+            human_counter = 1
+            print (f"{self.human.name} WINS!")
+        elif self.human.selected_item == "Scissors" and self.ai.selected_item == "Paper":
+            human_counter = 1
+            print (f"{self.human.name} WINS!")
+        elif self.human.selected_item == "Rock" and self.ai.selected_item == "Lizard":
+            human_counter = 1
+            print (f"{self.human.name} WINS!")
+        elif self.human.selected_item == "Lizard" and self.ai.selected_item == "Spock":
+            human_counter = 1
+            print (f"{self.human.name} WINS!")
+        elif self.human.selected_item == "Spock" and self.ai.selected_item == "Scissors":
+            human_counter = 1
+            print (f"{self.human.name} WINS!")
+        elif self.human.selected_item == "Scissors" and self.ai.selected_item == "Lizard":
+            human_counter = 1
+            print (f"{self.human.name} WINS!")
+        elif self.human.selected_item == "Lizard" and self.ai.selected_item == "Paper":
+            human_counter = 1
+            print (f"{self.human.name} WINS!")
+        elif self.human.selected_item == "Paper" and self.ai.selected_item == "Spock":
+            human_counter = 1
+            print (f"{self.human.name} WINS!")
+        elif self.human.selected_item == "Spock" and self.ai.selected_item == "Rock":
+            human_counter = 1
+            print (f"{self.human.name} WINS!")
+        else:
+            print (f"{self.ai.name} WINS!")
+            computer_counter = 1
+        return human_counter
+
+        pass
+
+    def players_choose_item_round_two (self):
+        print("Round 2 is under way")
         self.human.choose_item()
         self.ai.choose_item()
         pass
@@ -27,5 +77,6 @@ class Gameplay:
              print (rules)
 
         pass
-    
+
+  
     
--- human_player.py
+++ human_player.py
@@ -9,7 +9,7 @@ class Human (Player):
         for item in self.item_list:
             print (f"Press {self.item_list.index(item) + 1} for {item}")
 
-        user_input = int (input (" Human make a choice "))
+        user_input = int (input ("Human make a choice "))
         self.selected_item = self.item_list [user_input - 1]
         print (f"{self.name} has chosen {self.selected_item}")
         
